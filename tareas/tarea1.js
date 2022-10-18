8866ab84595806343654128c7418abd6eb1e13fa
olimaritere <olimaritere@gmail.com>
2022-10-18 05:48:41 -0500

    task completed

--- tareas/tarea1.js
+++ tareas/tarea1.js
@@ -5,7 +5,18 @@ let num3 = parseInt(prompt("Ingresa el 3er número: "))
 
 console.log(num1, num2, num3);
 
-const numeros = ([num1, num2, num3]);
+let numeros = []
+
+numeros.push(num1, num2, num3);
+
+numeros.sort((a, b)=>a-b);
+console.log(numeros[0], numeros[1], numeros[2]);
+
+numeros.reverse();
+console.log(numeros[0], numeros[1], numeros[2]);
+
+
+
 
 
 
