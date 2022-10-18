86b3de9b0aaad638b8f29dfb56cef83e51666708
olimaritere <olimaritere@gmail.com>
2022-10-18 04:53:58 -0500

    AlgoritHm that puts numbers in order

---
+++ tareas/tarea1.js
@@ -0,0 +1,13 @@
+let num1 = parseInt(prompt("Ingresa el 1er número: "))
+let num2 = parseInt(prompt("Ingrese el 2do numero: "))
+let num3 = parseInt(prompt("Ingresa el 3er número: "))
+
+
+console.log(num1, num2, num3);
+
+const numeros = ([num1, num2, num3]);
+
+
+
+
+
