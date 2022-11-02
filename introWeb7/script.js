e573facee66b4404ed04f59ce1d891c7bcd23cd0
olimaritere <olimaritere@gmail.com>
2022-11-02 11:04:00 -0600

    Java script

---
+++ introWeb7/script.js
@@ -0,0 +1,112 @@
+//Hola
+/*Hola
+como estás*/
+
+console.log("Lo que tú quieras")
+alert("Alerta de chisme")
+
+console.log("Esta es una suma", 5+5) ;
+
+let fName="Olivia";
+let lName="Suarez";
+let age=30;
+let donador=false;
+let bloodType= undefined;
+let children=null;
+
+console.log(fName)
+console.log(lName)
+console.log(age)
+console.log(donador)
+console.log(bloodType)
+console.log(children)
+
+
+var birthDay = 1992;
+var actualYear = 2022;
+
+console.log("Olivia")
+console.log(actualYear - birthDay)
+
+console.log("David")
+var birthDay = 1993;
+console.log(actualYear - birthDay)
+
+console.log("Andi")
+var birthDay = 1998;
+console.log(actualYear - birthDay)
+
+console.log("Ana Pau")
+var birthDay = 1998;
+console.log(actualYear - birthDay)
+
+console.log("Max")
+var birthDay = 1999
+console.log(actualYear - birthDay)
+
+console.log ("Alan")
+var birthDay = 1996;
+console.log(actualYear - birthDay)
+
+console.log(Number(false));
+console.log(String(false));
+console.log(Boolean(false));
+
+console.log(Number(true));
+console.log(String(true));
+console.log(Boolean(true));
+
+console.log(Number(0));
+console.log(String(0));
+console.log(Boolean(0));
+
+console.log(Number(1));
+console.log(String(1));
+console.log(Boolean(1));
+
+console.log(Number("0"));
+console.log(String("0"));
+console.log(Boolean("0"));
+
+console.log(Number("000"));
+console.log(String("000"));
+console.log(Boolean("000"));
+
+console.log(Number("1"));
+console.log(String("1"));
+console.log(Boolean("1"));
+
+console.log(Number(NaN));
+console.log(String(NaN));
+console.log(Boolean(NaN));
+
+console.log(Number(Infinity));
+console.log(String(Infinity));
+console.log(Boolean(Infinity));
+
+console.log(Number(""));
+console.log(String(""));
+console.log(Boolean(""));
+
+console.log(Number("20"));
+console.log(String("20"));
+console.log(Boolean("20"));
+
+console.log(Number("twenty"));
+console.log(String("twenty"));
+console.log(Boolean("twenty"));
+
+console.log(Number(null));
+console.log(String(null));
+console.log(Boolean(null));
+
+console.log(Number(undefined));
+console.log(String(undefined));
+console.log(Boolean(undefined));
+
+
+
+
+
+
+
